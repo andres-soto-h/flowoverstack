44fdc93a8a5557918ce364f338c35e83aad38a2d
andres-soto-h <andres.soto.h@outlook.com>
2018-08-24 07:15:30 -0500

    adding answers and relationships to database

--- flowoverstack/app/models/question.rb
+++ flowoverstack/app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ApplicationRecord
     validates :title, :body, presence: true
     belongs_to :user
     has_many :comments, as: :commentable
+    has_many :answers, through: :users
 end
