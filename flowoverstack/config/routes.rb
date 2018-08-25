814e2a11953d098d23db24b36eb2ca6ac6809297
andres-soto-h <andres.soto.h@outlook.com>
2018-08-25 06:42:59 -0500

    add route for creating new answer

--- flowoverstack/config/routes.rb
+++ flowoverstack/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'questions/index'
   root 'questions#index'
   resources :questions
-  resources :answers
+  #resources :answers
+  post 'answers/create'
 
   post 'comments/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
